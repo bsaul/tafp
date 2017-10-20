b21cdf861ce538306524df875d88332b968f5c12
Bradley Saul <bradleysaul@gmail.com>
2017-10-20 15:30:37 -0400

    creates a function to import data

--- R/retrieval_functions.R
+++ R/retrieval_functions.R
@@ -1,13 +1,16 @@
 #------------------------------------------------------------------------------#
 #' Retrieve and save RedCap data
 #'
+#' @param token RedCap token
 #' 
+#' @export
 #------------------------------------------------------------------------------#
 
 retrieve_redcap_data <- function(token){
   today    <- Sys.Date()
-  all_data <- redcap_read(redcap_uri="https://redcap.fasting.org/redcap_v6.8.1/API/",
-                          token=token)
-  
-  save(all_data, file = paste0("inst/raw_data/redcap_retrieval/tasp_all_data_", today, ".rda"))
+  outname  <- paste0("tasp_retrieval_", today)
+  x <- redcap_read(redcap_uri="https://redcap.fasting.org/redcap_v6.8.1/API/",
+                   token=token)
+  assign(outname, x)
+  save(list = outname, file = paste0("inst/raw_data/redcap_retrieval/", outname, ".rda"))
 }
