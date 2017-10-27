9210acb5e09fb37e8e84f8e6dd1eaa2d062eaf5c
Bradley Saul <bradleysaul@gmail.com>
2017-10-27 10:41:21 -0400

    further work on create_analysis_data_frame function. Still need to add post_process.

--- programs/1a_create_analysis_datasets.R
+++ programs/1a_create_analysis_datasets.R
@@ -1,43 +1,83 @@
 load("inst/raw_data/redcap_retrieval/tasp_retrieval_2017-10-20.rda")
 
-all_data <- `tasp_retrieval_2017-10-20`$data
+## ToDo
+# add post process capability for (e.g. removing variables)
 
+all_data <- `tasp_retrieval_2017-10-20`$data
 
 # Variable Settings
 key_vars <- c("record_id", "redcap_event_name")
 flq_vars <- 3:95
 dtrt_vars <- 130:190
 
-analysis_data_settings <- list()
+analysis_data_settings <- list(
+  flq = list(
+    data_name      = "flq",
+    description    = "A description",
+    key_variables  = key_vars,
+    keep_variables = flq_vars,
+    criteria       = quo(redcap_event_name %in% c("pretreatment_arm_1", "posttreatment_arm_1")),
+    checks         = list(
+      list(
+        with      = "included",
+        condition = function(x){ any(x$food_liking_questionnaire_complete != 2) },
+        response  = quote(warning("FLQ has incomplete forms")),
+        action    = function(x){print(x)}), 
+      list(
+        with      = "excluded",
+        condition = function(x){!(all(is.na(x[ , -(1:2)])))},
+        response  = quote(warning("Food liking questionnaire has responses where there should be none")),
+        action    = function(x){print(x[ , -(1:2)])}))),
+  dtrt = list(
+    data_name   = "dtrt",
+    description = "A description",
+    key_variables  = key_vars,
+    keep_variables = dtrt_vars,
+    criteria       = quo(redcap_event_name %in% c("pretreatment_arm_1", "posttreatment_arm_1")),
+    checks         = list(
+      list(
+        with      = "included",
+        condition = function(x){ any(x$detection_recognition_assays_complete != 2) },
+        response  = quote(warning("Detection and Recognition has incomplete forms")),
+        action    = function(x){print(x)}), 
+      list(
+        with      = "excluded",
+        condition = function(x){!(all(is.na(x[ , -(1:2)])))},
+        response  = quote(warning("Detection and Recognition has responses where there should be none")),
+        action    = function(x){print(x[ , -(1:2)])})
+    ))
+)
+
+
+create_analysis_data_frame <- function(input_data, settings){
+  # 1. Select variables
+  basis_data <- input_data %>%
+    dplyr::select(settings$key_variables, settings$keep_variables)
+  
+  # 2. Apply criteria filter
+  basis_data %>% 
+    dplyr::filter(!!settings$criteria) ->
+    out_data
+    
+  excluded_data <- basis_data %>%
+    dplyr::filter(!(!!settings$criteria))
+  
+  # 3. Apply checks
+  lapply(settings$checks, function(check) {
+    data_to_check <- if(check$with == "included") out_data else excluded_data
+    if(check$condition(data_to_check)){
+      eval(check$action(data_to_check))
+      eval(check$response)
+    }
+  })
+  
+  out_data 
+}
+
+
 
 #### flq = Food Liking questionnair
-
-# create food liking dataset
-create_flq_dataset <- function(all_data, key_vars, dataset_vars){
-  start <- all_data %>%
-    dplyr::select(key_vars, dataset_vars)
-  
-  # FLQ should only be completed at 2 timepoints: pre and post treatment
-  include <- start %>%  
-    dplyr::filter(redcap_event_name %in% c("pretreatment_arm_1", "posttreatment_arm_1")) 
-  
-  if(any(include$food_liking_questionnaire_complete != 2)){
-    warning("FLQ has incomplete forms")
-  }
-  
-  include <- include %>%
-    select(-food_liking_questionnaire_complete, -p2_example)
-  
-  # Check for responses after pretreatment and before posttreatment
-  exclude <- start %>%
-    dplyr::filter(!(redcap_event_name %in% c("pretreatment_arm_1", "posttreatment_arm_1")))
-  
-  if(!(all(is.na(exclude[ , -(1:2)])))){
-    warning("Food liking questionnaire has responses where there should be none")
-  }
-  
-  return(include)
-}
+create_analysis_data_frame(all_data, analysis_data_settings$flq)
 
 # create long flq dataset
 flq <- create_flq_dataset(all_data, key_vars, flq_vars)
@@ -52,25 +92,27 @@ flq_long <- flq %>%
   )
 
 #### dtrt = recognition and detection Liking questionnaire
+create_analysis_data_frame(all_data, analysis_data_settings$dtrt)
 
-start <- all_data %>%
-  dplyr::select(key_vars, dtrt_vars)
 
-# dtrt should not be completed at 2 timepoints: pre and post treatment
-include <- start %>%  
-  dplyr::filter(!(redcap_event_name %in% c("pretreatment_arm_1", "posttreatment_arm_1")))
-
-if(any(include$detection_recognition_assays_complete != 2)){
-  warning("Detection and Recognition has incomplete forms")
-}
-
-include <- include %>%
-  select(-detection_recognition_assays_complete)
-
-# Check for responses after pretreatment and before posttreatment
-exclude <- start %>%
-  dplyr::filter(redcap_event_name %in% c("pretreatment_arm_1", "posttreatment_arm_1"))
-
-if(!(all(is.na(exclude[ , -(1:2)])))){
-  warning("Detection and Recognition has responses where there should be none")
-}
+# start <- all_data %>%
+#   dplyr::select(key_vars, dtrt_vars)
+# 
+# # dtrt should not be completed at 2 timepoints: pre and post treatment
+# include <- start %>%  
+#   dplyr::filter(!(redcap_event_name %in% c("pretreatment_arm_1", "posttreatment_arm_1")))
+# 
+# if(any(include$detection_recognition_assays_complete != 2)){
+#   warning("Detection and Recognition has incomplete forms")
+# }
+# 
+# include <- include %>%
+#   select(-contains("complet"))
+# 
+# # Check for responses after pretreatment and before posttreatment
+# exclude <- start %>%
+#   dplyr::filter(redcap_event_name %in% c("pretreatment_arm_1", "posttreatment_arm_1"))
+# 
+# if(!(all(is.na(exclude[ , -(1:2)])))){
+#   warning("Detection and Recognition has responses where there should be none")
+# }
