de8c6eeac067b0e3c3ffddba004ce3e4f6c95690
Bradley Saul <bradleysaul@gmail.com>
2017-11-03 16:09:37 -0400

    change date format

--- R/retrieval_functions.R
+++ R/retrieval_functions.R
@@ -7,9 +7,9 @@
 #------------------------------------------------------------------------------#
 
 retrieve_redcap_data <- function(token){
-  today    <- Sys.Date()
+  today    <- format(Sys.Date(), "%Y%m%d")
   outname  <- paste0("tasp_retrieval_", today)
-  x <- redcap_read(redcap_uri="https://redcap.fasting.org/redcap_v6.8.1/API/",
+  x <- REDCapR::redcap_read(redcap_uri="https://redcap.fasting.org/redcap_v6.8.1/API/",
                    token=token)
   assign(outname, x)
   save(list = outname, file = paste0("inst/raw_data/redcap_retrieval/", outname, ".rda"))
