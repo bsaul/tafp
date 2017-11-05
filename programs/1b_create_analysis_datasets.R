95692065fe8ee7b6b49d5f73d35fbd015b9f81e6
Bradley Saul <bradleysaul@gmail.com>
2017-11-05 17:43:49 -0500

    creates supra_long dataset

--- programs/1b_create_analysis_datasets.R
+++ programs/1b_create_analysis_datasets.R
@@ -11,29 +11,22 @@
 tasp_data_20171103 <- within(tasp_data_20171103, {
   # create long flq dataset ####
   flq_long <- flq %>%
-    dplyr::select(-gender_flq, -diet_flq, -date_flq, -age_flq) %>%
+    dplyr::select(-gender_flq, -diet_flq, -contains("date"), -age_flq) %>%
     tidyr::gather(key = "variable", value = "response", 
-                  -record_id, -redcap_event_name) %>%
+                  -record_id, -time) %>%
     dplyr::mutate(
       part = substr(variable, 2, 2),
       question = stringr::str_extract(variable, "q.*") %>% stringr::str_replace("q", "")
     ) %>%
     dplyr::arrange(
-      record_id, redcap_event_name
+      record_id, time
     )
   
   # create long dtrt dataset ####
   dtrt_long_blind <- dtrt %>% 
-    dplyr::mutate(
-      time = case_when(
-        redcap_event_name == "day_0_arm_1" ~ 1,
-        redcap_event_name == "day_5_arm_1" ~ 2,
-        redcap_event_name == "final_fasting_day_arm_1" ~ 3,
-        redcap_event_name == "refeed_day_3_arm_1" ~ 4)
-    ) %>%
-    dplyr::select(-contains("comment"), -date_01, -date_02) %>%
+    dplyr::select(-contains("comment"), -contains("date")) %>%
     tidyr::gather(key = "variable", value = "response", 
-                  -record_id, -redcap_event_name, -randomization_id_rtdt, -time) %>%
+                  -record_id, -randomization_id_rtdt, -time) %>%
     dplyr::mutate(
       variable2      = stringr::str_replace(variable, "(_0.)$", ""),
       variable_name  = stringr::str_extract(variable, "(group|sample|recognition_taste)" ),
@@ -43,15 +36,14 @@ tasp_data_20171103 <- within(tasp_data_20171103, {
                                if_else(variable_name == "sample", "cup_id", variable_name))
     ) %>%
     dplyr::select(-variable, -variable2) %>%
-    dplyr::arrange(record_id, redcap_event_name, taste_position, level, variable_name) %>%
+    dplyr::arrange(record_id, time, taste_position, level, variable_name) %>%
     
-    tidyr::unite(temp, record_id, redcap_event_name, time,
+    tidyr::unite(temp, record_id, time,
                  randomization_id_rtdt, taste_position, level, sep = ":::") %>%
     dplyr::group_by(temp) %>%
-    # dplyr::mutate(id = 1:n()) %>%
     tidyr::spread(variable_name, response) %>%
     tidyr::separate(temp, sep = ":::",
-                    into = c("record_id", "redcap_event_name", "time", 
+                    into = c("record_id", "time", 
                              "randomization_id", "taste_position", "level")) %>%
     dplyr::mutate(
       taste_position = as.integer(taste_position) - 1,
@@ -59,10 +51,37 @@ tasp_data_20171103 <- within(tasp_data_20171103, {
       time           = as.integer(time),
       chosen_cup_position = chosen_cup_position + 1
     ) %>%
-    dplyr::arrange(record_id, redcap_event_name, taste_position, level)
+    dplyr::arrange(record_id, time, taste_position, level)
+  
+  # Create long supra dataset ####
+  supra_long_blind <- tasp_data_20171103$supra %>% 
+    dplyr::select(-contains("comment"), -contains("date_st")) %>%
+    dplyr::group_by(record_id, randomization_id_st, time) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(
+      data = purrr::map(data, 
+      ~ .x %>% 
+        tidyr::gather(key = "variable", value = "response") %>%
+        dplyr::mutate(
+          cup_order      = as.integer(stringr::str_extract(variable, "\\d")),
+          taste_position = as.integer(stringr::str_extract(variable, "0.$")),
+          variable       = if_else(grepl("taste", variable), "flavor_tasted", "response" )
+        ) %>%
+        dplyr::group_by(taste_position) %>%
+        dplyr::mutate(
+          flavor_tasted = max(response[variable == "flavor_tasted"])
+        ) %>%
+        dplyr::filter(
+          variable == "response"
+        ) %>%
+        dplyr::select(-variable))
+    ) %>%
+    tidyr::unnest() %>%
+    dplyr::select(randomization_id = randomization_id_st, everything())
+  
 })
 
 
 
 
-
+tasp_data_20171103$supra_long_blind
