c35639988ccc7c36d00fe1c7aeb3184e2747aa03
Bradley Saul <bradleysaul@gmail.com>
2017-11-10 10:29:03 -0500

    use getPass function to grab token

--- R/retrieval_functions.R
+++ R/retrieval_functions.R
@@ -6,11 +6,11 @@
 #' @export
 #------------------------------------------------------------------------------#
 
-retrieve_redcap_data <- function(token){
+retrieve_redcap_data <- function(){
   today    <- format(Sys.Date(), "%Y%m%d")
   outname  <- paste0("tasp_retrieval_", today)
   x <- REDCapR::redcap_read(redcap_uri="https://redcap.fasting.org/redcap_v6.8.1/API/",
-                   token=token)
+                   token=getPass::getPass(msg = "Enter RedCap token: "))
   assign(outname, x)
   save(list = outname, file = paste0("inst/raw_data/redcap_retrieval/", outname, ".rda"))
 }
